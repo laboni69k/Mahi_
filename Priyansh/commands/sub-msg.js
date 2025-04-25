0612aee66047644686a6c3ecee3b0b6584a8e819
fahim55k <fahimvaiya91@gmail.com>
2025-04-25 11:57:24 +0600

    Update sub-msg.js

--- Priyansh/commands/sub-msg.js
+++ Priyansh/commands/sub-msg.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("Priyansh rajput")==0 || event.body.indexOf("Sub")==0 || event.body.indexOf("Subscribe")==0 || event.body.indexOf("Priyansh")==0) {
 		var msg = {
-				body: "👋For Any Kind Of Help Contact On Telegram  Username 👉 @Priyanshrajput😇",
+				body: "👋For Any Kind Of Help Contact On Telegram  Username 👉 @Fɑhɪm ✨🔥",
 				attachment: fs.createReadStream(__dirname + `/noprefix/sub.mp3`)
 			}
 			api.sendMessage(msg, threadID, messageID);
